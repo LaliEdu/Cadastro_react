465cd30d59c90432c4d599184332244762910e21
Eduarda Alves <alvesneivam1203@gmail.com>
2025-04-08 11:33:01 -0300

    Update v1

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,25 @@
+import './style.css'
+
+function Home() {
+
+  return (
+    <div>
+      <from action>
+      <h1>Cadastrar Usuarios</h1>
+      <input type="text" name="name" id="" placeholder='Entre com seu nome' />
+      <input type="text" name="email" id="" placeholder='Entre com seu email' />
+      <input type="number=" name="age" id=""/>
+      <button type="button">Cadastrar</button>
+      </from>
+    <div>
+      <div>
+        <p>Nome:</p>
+        <p>Email:</p>
+        <p>Age</p>
+      <div>
+    </div>
+      
+
+  }
+   
+export default Home
